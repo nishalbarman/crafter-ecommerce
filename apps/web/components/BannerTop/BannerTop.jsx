950f76de716bcbced96baf9b9d700813ea2108a8
Nishal Barman <helptechnoapps@gmail.com>
2024-01-31 17:59:50 +0530

    Redux changes

--- apps/web/components/BannerTop/BannerTop.jsx
+++ apps/web/components/BannerTop/BannerTop.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import Carousel from "../Carousel/Carousel";
 
--- packages/redux/store.js
+++ packages/redux/store.js
@@ -1,27 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { cartReducer } from "./slices/cartSlice";
-import { wishlistReducer } from "./slices/wishlistSlice";
-import { bannerReducer } from "./slices/bannerSlice";
-import { updateBanner } from "./slices/bannerSlice";
-import { keywordReducer } from "./slices/keywordsSlice";
-import { categoryReducer } from "./slices/categorySlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { cartReducer } from "./slices/cartSlice";
+// import { wishlistReducer } from "./slices/wishlistSlice";
+// import { bannerReducer } from "./slices/bannerSlice";
+// import { updateBanner } from "./slices/bannerSlice";
+// import { keywordReducer } from "./slices/keywordsSlice";
+// import { categoryReducer } from "./slices/categorySlice";
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    wishlist: wishlistReducer,
-    banner: bannerReducer,
-    keywords: keywordReducer,
-    category: categoryReducer,
-  },
-});
+// const store = configureStore({
+//   reducer: {
+//     cart: cartReducer,
+//     wishlist: wishlistReducer,
+//     banner: bannerReducer,
+//     keywords: keywordReducer,
+//     category: categoryReducer,
+//   },
+// });
 
-export {
-  bannerReducer,
-  cartReducer,
-  categoryReducer,
-  keywordReducer,
-  wishlistReducer,
-  updateBanner,
-  store,
-};
+// export {
+//   bannerReducer,
+//   cartReducer,
+//   categoryReducer,
+//   keywordReducer,
+//   wishlistReducer,
+//   updateBanner,
+//   store,
+// };
