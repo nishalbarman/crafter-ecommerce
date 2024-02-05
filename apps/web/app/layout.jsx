c38461238ffd6eafe4441ce11323cb4c9017cd1a
Nishal Barman <helptechnoapps@gmail.com>
2024-02-05 13:27:06 +0530

    Wishlist page added

--- apps/web/app/layout.jsx
+++ apps/web/app/layout.jsx
@@ -1,3 +1,5 @@
+import ReduxStore from "../components/ReduxStore/ReduxStore";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReduxStore>{children}</ReduxStore>
+      </body>
     </html>
   );
 }
--- packages/redux/slices/bannerSlice.js
+++ packages/redux/slices/bannerSlice.js
@@ -4,6 +4,13 @@ const initialState = {
   totalItems: 0,
   bannerItems: [
     {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
       imageUrl:
         "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
       altText: "Normal picture of a banner",
@@ -19,7 +26,145 @@ const initialState = {
       description: "A beautiful scenario",
       path: "/ios",
     },
-    // Add more items as needed
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    {
+      imageUrl: "http://localhost:3000/banner.png",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/6mgkC9IaOSq5lrzyk11WYgz91nOMcrzri79lIsLSTNU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0VV/czRxTjNUcWNYOTU3/eWFwM0hEaTNrVGls/dm1ibGkwbFF1aTFr/bWpub1dmc19CV2pL/amVVSE1weWlXZjNz/OVAwTUZPT1k2a1pz/V3hqM3AxQ2doeXVx/d0xLc2pXSmN2Y2Fx/NWhTeE9iZUVqVHI5/WXZzZl9PNDFGX1Fo/VGlPbGVkNDBVUFhw/aEFEaG03S0JGRnN3/QmpwNGc",
+      altText: "Normal picture of a banner",
+      title: "Normal picture of a banner",
+      description: "Normal picture of a banner",
+      path: "/home",
+    },
+    {
+      imageUrl:
+        "https://imgs.search.brave.com/-g7c7St9_tIFkJTnsuQ0_jFFPPF6YVTMhDheNohdXB0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1wc2QvYmFu/bmVyLXRlbXBsYXRl/LW1lZ2Etc2FsZV8y/My0yMTQ4NzI5NTMw/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+      altText: "A beautiful scenario",
+      title: "A beautiful scenario",
+      description: "A beautiful scenario",
+      path: "/ios",
+    },
+    
   ],
 };
 
