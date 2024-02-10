6120eb9c512c5c6536172a81f8cdf9890b8d0fe9
Nishal Barman <helptechnoapps@gmail.com>
2024-02-10 10:11:39 +0530

    Trying to modify responsiveness for mobile devies

--- apps/web/components/BestSelling/BestSelling.jsx
+++ apps/web/components/BestSelling/BestSelling.jsx
@@ -5,9 +5,7 @@ import { getBackendUrl } from "../../helpter/utils";
 
 const getProducts = async () => {
   const backendUrl = getBackendUrl();
-  const response = await fetch(
-    `${backendUrl}api/v1/products?&page=1&limit=25`
-  );
+  const response = await fetch(`${backendUrl}api/v1/products?&page=1&limit=25`);
   const data = await response.json();
   return data.data;
 };
@@ -19,12 +17,12 @@ async function BestSelling() {
     <div className="w-full h-fit mt-10 lg:mt-[3rem]">
       <TitleWithBar title={"This Month"} />
       <div className="relative">
-        <div className="w-full flex justify-between items-center pt-8 pb-8 md:pt-7 md:pb-10">
+        <div className="w-full flex justify-between items-center pt-8 pb-8 md:pt-7 md:pb-10 max-sm:mt-[-10px]">
           <span className="text-2xl md:text-3xl font-andika font-bold">
             Best Selling Products
           </span>
           <div className="flex items-center justify-center">
-            <button className="pt-3 pb-3 pl-8 pr-8 text-sm md:pt-3 md:pb-3 md:pl-8 md:pr-8 rounded-lg bg-[#DB4444] text-white font-semibold font-andika cursor-poiner hover:scale-[1.05]">
+            <button className="pt-3 pb-3 pl-8 pr-8 text-sm md:pt-3 md:pb-3 md:pl-8 md:pr-8 rounded-lg bg-[#DB4444] text-white font-semibold font-andika cursor-poiner hover:scale-[1.05] max-sm:p-[10px_30px]">
               View All
             </button>
           </div>
