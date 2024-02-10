aafc179ece62a1917c42099b5fe169faa6290a3e
Nishal Barman <helptechnoapps@gmail.com>
2024-02-10 10:11:39 +0530

    Trying to modify responsiveness for mobile devies

--- apps/web/components/TitleWithBar/TitleWithBar.jsx
+++ apps/web/components/TitleWithBar/TitleWithBar.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 function TitleWithBar({ title }) {
   return (
-    <div className="w-full flex gap-2 items-center ">
-      <Image src="/assets/red_bar.svg" height={23} width={18} alt="red bar"/>
+    <div className="w-full flex gap-2 items-center max-sm:mb-[-10px]">
+      <Image src="/assets/red_bar.svg" height={23} width={18} alt="red bar" />
       <span className="text-[18px] text-[#DB4444] font-semibold">{title}</span>
     </div>
   );
