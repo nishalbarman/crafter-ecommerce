9c7e263746016aee02cccc5a0affe0f4ebb519c2
Nishal Barman <helptechnoapps@gmail.com>
2024-02-10 09:51:34 +0530

    Trying to modify product items

--- apps/web/components/ExploreProducts/AllProducts.jsx
+++ apps/web/components/ExploreProducts/AllProducts.jsx
@@ -25,7 +25,7 @@ function AllProducts({ exploreProductData }) {
   ] = useDeleteWishlistMutation();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center gap-4 items-stretch">
+    <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center min-md:gap-4 items-stretch">
       {exploreProductData?.map((item, index) => (
         <div
           className="w-full h-fit flex flex-row justify-center p-1"
