91fcb83e5059a7d7bf7e093d83cff194e196faf7
Nishal Barman <helptechnoapps@gmail.com>
2024-02-13 09:45:23 +0530

    Build fix, added dynamic config for every payu route

--- apps/web/app/api/v1/payment/payu/cart-hash/route.js
+++ apps/web/app/api/v1/payment/payu/cart-hash/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 import { Cart, Order } from "../../../../../../models/models";
