0b378a2031e0502138d46014d700dc3928368717
Nishal Barman <helptechnoapps@gmail.com>
2024-02-15 00:15:32 +0530

    build command failed fix:1

--- apps/web/app/redirect/page.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import Redirect from "../../components/Redirect/Redirect";
-
-function page({ searchParams }) {
-  const path = searchParams.path;
-  return <Redirect path={path} />;
-}
-
-export default page;
