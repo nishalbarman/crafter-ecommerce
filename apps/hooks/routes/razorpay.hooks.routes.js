7f5f86b458b2825bf957767d2b1d519c4918e2d1
Nishal Barman <helptechnoapps@gmail.com>
2024-02-16 10:52:49 +0530

    razorpay order id generation route created

--- apps/hooks/routes/razorpay.hooks.routes.js
+++ apps/hooks/routes/razorpay.hooks.routes.js
@@ -4,6 +4,18 @@ const router = Router();
 
 router.get("/order_success", (req, res) => {
   try {
+    console.log("Get so its an get request");
+    console.log(req.body);
+    res.status(200).json({ status: true, message: error.message });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: false, message: error.message });
+  }
+});
+
+router.get("/order_success", (req, res) => {
+  try {
+    console.log("Get so its an post request");
     console.log(req.body);
     res.status(200).json({ status: true, message: error.message });
   } catch (error) {
